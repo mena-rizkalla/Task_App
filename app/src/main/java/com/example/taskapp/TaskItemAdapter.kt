1d88c28fb5b26ca9e77b630a1f5c1b10519ff591
mena <mena.rizkalla1@gmail.com>
2022-12-16 21:07:26 +0200

    use dataBinding

--- app/src/main/java/com/example/taskapp/TaskItemAdapter.kt
+++ app/src/main/java/com/example/taskapp/TaskItemAdapter.kt
@@ -1,35 +1,31 @@
 package com.example.taskapp
 
-import android.content.Context
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import android.widget.CheckBox
 import android.widget.TextView
-import android.widget.Toast
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import java.lang.reflect.Array.set
+import com.example.taskapp.databinding.TaskItemBinding
 
 class TaskItemAdapter() : ListAdapter<Task,TaskItemAdapter.ViewHolder>(TaskDiffItemCallback()) {
 
 
-
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.task_item,parent,false)
-        return ViewHolder(view)
+        val binding = TaskItemBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return ViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val task = getItem(position)
-        holder.taskName.text = task.taskName
-        holder.taskDone.isChecked = task.taskDone
-
+        holder.bind(task)
     }
 
 
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val taskName = itemView.findViewById<TextView>(R.id.task)
-        val taskDone = itemView.findViewById<CheckBox>(R.id.task_done)
+    class ViewHolder(val binding: TaskItemBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(task: Task){
+            binding.taskData =task
+        }
+
     }
 }
